5f9d9ebd3ed2fbb8c775841eaff547a20c8909f9
Guatoncito <benja_as@hotmail.com>
2014-10-06 21:54:00 -0300

    lo subí a la wea

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from numpy import *
-matriz=zeros(5,6)
+matriz=zeros((5,6))
 for i in range(5):
         for j in range(6):
-                matriz[i,j]=9
+                matriz[i,j]='B'
 evitar=int(raw_input("Ingrese casilla para evitar:"))
 if evitar<5:
-        matriz[1,evitar]=6
+        matriz[1,evitar]='P'
 elif evitar<9:
-        matriz[2,evitar-4]=6
+        matriz[2,evitar-4]='P'
 else:
-        matriz[3,evitar-8]=6
+        matriz[3,evitar-8]='P'
 ganar=raw_input("Ingrese casilla a la cual desea llegar:")
 if ganar<5:
-        matriz[1,ganar]=7
+        matriz[1,ganar]='G'
 elif evitar<9:
-        matriz[2,ganar-4]=7
+        matriz[2,ganar-4]='G'
 else:
-        matriz[3,ganar-8]=7
+        matriz[3,ganar-8]='G'
 perder=raw_input("Ingrese casilla no accesible:")
 if perder<5:
-        matriz[1,perder]=8
+        matriz[1,perder]='M'
 elif perder<9:
-        matriz[2,perder-4]=8
+        matriz[2,perder-4]='M'
 else:
-        matriz[3,perder-8]=8
+        matriz[3,perder-8]='M'
 inicio=raw_input("Ingrese casilla de inicio:")
 if inicio<5:
-        matriz[1,inicio]=5
+        matriz[1,inicio]='I'
 elif inicio<9:
-        matriz[2,inicio-4]=5
+        matriz[2,inicio-4]='I'
 else:
-        matriz[3,inicio-8]=5
-print matriz
+        matriz[3,inicio-8]='I'
